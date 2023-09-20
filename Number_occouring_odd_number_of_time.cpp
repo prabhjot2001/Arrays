b49ca0d7a87531020a0342b754985d6845fc5198
prabhjot2001 <lazycoder001@gmail.com>
2023-09-20 23:49:15 +0530

    using map

--- Number_occouring_odd_number_of_time.cpp
+++ Number_occouring_odd_number_of_time.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/* ----------- PROBLEM STATMENT--------------
+ Given an array of where each element is occouring even no. of time only one element is occouring odd number of time. find that element */
 int main(){
+  int arr[] = {1,2,3,1,2,3,3};
+  int n = sizeof(arr)/ sizeof(arr[0]);
+  map<int,int>mp;
+  for(int i=0;i<n;i++){
+      mp[arr[i]]++;
+  }
+  for(auto it=mp.begin(); it!=mp.end();it++)
+  {
+    if(it->second %2!= 0){
+        cout<<it->first;
+        break;
+    }
+  }
   
 }
